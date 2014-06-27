268034b4427f6ae1041466989d482e0cd3277e4d
Alex Rouse <alex.rouse@raizlabs.com>
2014-06-27 10:14:23 -0400

    Adding frameworks and submodules

---
+++ RZFuelSource/RZFuelSource/Data/RZFuelStation.h
@@ -0,0 +1,13 @@
+//
+//  RZFuelStation.h
+//  RZFuelSource
+//
+//  Created by alex.rouse on 6/27/14.
+//  Copyright (c) 2014 Raizlabs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RZFuelStation : NSObject
+
+@end
---
+++ RZFuelSource/RZFuelSource/Data/RZFuelWebService.m
@@ -0,0 +1,13 @@
+//
+//  RZFuelWebService.m
+//  RZFuelSource
+//
+//  Created by alex.rouse on 6/27/14.
+//  Copyright (c) 2014 Raizlabs. All rights reserved.
+//
+
+#import "RZFuelWebService.h"
+
+@implementation RZFuelWebService
+
+@end
